1043a7d5bde0680613e3e9217ed490adfaa54e64
jheissat <julien.heissat@gmail.com>
2015-03-27 18:31:11 +0100

    Initial commit

---
+++ platforms/ios/www/js/homeview.js
@@ -0,0 +1,42 @@
+
+(function () {
+
+    /* ---------------------------------- Local Variables ---------------------------------- */
+    var homeTpl = Handlebars.compile($("#home-tpl").html());
+    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
+    var service = new EmployeeService();
+    service.initialize().done(function () {
+        renderHomeView();
+    });
+
+    /* --------------------------------- Event Registration -------------------------------- */
+    document.addEventListener('deviceready', function () {
+        StatusBar.overlaysWebView( false );
+        StatusBar.backgroundColorByHexString('#ffffff');
+        StatusBar.styleDefault();
+        FastClick.attach(document.body);
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
+    }, false);
+
+    /* ---------------------------------- Local Functions ---------------------------------- */
+    function findByName() {
+        service.findByName($('.search-key').val()).done(function (employees) {
+            $('.content').html(employeeListTpl(employees));
+        });
+    }
+
+    function renderHomeView() {
+        $('body').html(homeTpl());
+        $('.search-key').on('keyup', findByName);
+    }
+
+}());
